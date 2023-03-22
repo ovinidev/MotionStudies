35003f2650ac3f8ca8642360a21719b05cfad747
ovinidev <vinimedeiros7@outlook.com>
2023-03-22 17:30:11 -0300

    feat: add on reveal component

--- src/pages/Scroll.tsx
+++ src/pages/Scroll.tsx
@@ -1,195 +1,46 @@
-import { Box, Circle, Flex, Heading } from "@chakra-ui/react";
-import { gsap } from "gsap";
-import { useEffect, useRef } from "react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-interface SectionProps {
-  className: string;
-}
-
-const Section = ({ className }: SectionProps) => {
-  gsap.registerPlugin(ScrollTrigger);
-
-  useEffect(() => {
-    gsap.fromTo(
-      className,
-      {
-        opacity: 0,
-        y: 100,
-      },
-      {
-        y: 0,
-        opacity: 1,
-        duration: 1,
-        scrollTrigger: {
-          trigger: className,
-          markers: true,
-          start: "top 80% ",
-          end: "bottom 70% ",
-          scrub: 1,
-          // play, pause, resume, reverse, restart, reset, complete
-          // toggleActions: "OnEnter OnLeave OnEnterBack OnLeaveBack",
-        },
-      }
-    );
-
-    return () => {
-      gsap.killTweensOf(className);
-    };
-  }, []);
-
-  return (
-    <Flex>
-      <Flex h={100} w={100} direction="column" bg="red" className="text">
-        <Heading>Scroll Trigger</Heading>
-      </Flex>
-      <Flex h={100} w={100} direction="column" bg="red" className="text">
-        <Heading>Scroll Trigger</Heading>
-      </Flex>
-    </Flex>
-  );
-};
+import { Flex, HStack, Stack } from "@chakra-ui/react";
+import { BoxMotionReveal } from "../components/BoxMotionReveal";
 
 export const Scroll = () => {
-  gsap.registerPlugin(ScrollTrigger);
-
-  useEffect(() => {
-    gsap.fromTo(
-      ".text",
-      {
-        opacity: 0,
-        y: 100,
-      },
-      {
-        y: 0,
-        opacity: 1,
-        duration: 1,
-        scrollTrigger: {
-          trigger: ".text",
-          markers: true,
-          start: "top 80% ",
-          end: "bottom 70% ",
-          scrub: 1,
-          // play, pause, resume, reverse, restart, reset, complete
-          // toggleActions: "OnEnter OnLeave OnEnterBack OnLeaveBack",
-        },
-      }
-    );
-
-    gsap.fromTo(
-      ".text2",
-      {
-        x: -800,
-      },
-      {
-        x: 0,
-        duration: 5,
-        scrollTrigger: {
-          trigger: ".text2",
-          markers: true,
-          start: "top 80% ",
-          end: "bottom 0% ",
-          scrub: 2,
-          // play, pause, resume, reverse, restart, reset, complete
-          // toggleActions: "OnEnter OnLeave OnEnterBack OnLeaveBack",
-        },
-      }
-    );
-
-    gsap.to(".pin", {
-      x: 0,
-      duration: 5,
-      scrollTrigger: {
-        trigger: ".pin",
-        markers: true,
-        start: "top 80% ",
-        end: "bottom 0% ",
-        scrub: true,
-        pin: true,
-        // play, pause, resume, reverse, restart, reset, complete
-        // toggleActions: "OnEnter OnLeave OnEnterBack OnLeaveBack",
-      },
-    });
-
-    return () => {
-      gsap.killTweensOf(".text");
-    };
-  }, []);
-
   return (
     <Flex direction="column" w="100%" flexDir="column" align="center">
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading className="text2">Scroll Trigger</Heading>
-      <Heading className="text2">Scroll Trigger</Heading>
-      <Heading className="text2">Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Circle className="pin" size="100px" bg="red">
-        PIN
-      </Circle>
-      <Heading mt="40rem">Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading className="text2">Scroll Trigger</Heading>
-      <Heading className="text2">Scroll Trigger</Heading>
-      <Heading className="text2">Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Circle className="pin" size="100px" bg="red">
-        PIN
-      </Circle>
-      <Heading mt="40rem">Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
-      <Heading>Scroll Trigger</Heading>
+      <Stack spacing="8" w="80%">
+        <Flex bg="gray.200" h="40rem" />
+        <Flex bg="gray.200" h="40rem" />
 
-      <Section className="text1" />
-      <Section className="text2" />
-      <Section className="text3" />
-      <Section className="text4" />
-      <Section className="text5" />
-      <Section className="text6" />
-      <Section className="text7" />
-      <Section className="text8" />
-      <Heading className="text9">Scroll Trigger</Heading>
+        <Flex bg="gray.200" h="40rem" />
+      </Stack>
+
+      <Stack spacing="12" my="2rem">
+        <BoxMotionReveal className="text">a</BoxMotionReveal>
+        <BoxMotionReveal className="text">a</BoxMotionReveal>
+        <BoxMotionReveal className="text">a</BoxMotionReveal>
+        <BoxMotionReveal className="text">a</BoxMotionReveal>
+        <BoxMotionReveal className="text">a</BoxMotionReveal>
+        <BoxMotionReveal className="text">a</BoxMotionReveal>
+        <BoxMotionReveal className="text">a</BoxMotionReveal>
+        <BoxMotionReveal className="text">a</BoxMotionReveal>
+      </Stack>
+
+      <HStack spacing="6" my="2rem">
+        <BoxMotionReveal className="text">a</BoxMotionReveal>
+        <BoxMotionReveal className="text">a</BoxMotionReveal>
+        <BoxMotionReveal className="text">a</BoxMotionReveal>
+        <BoxMotionReveal className="text">a</BoxMotionReveal>
+        <BoxMotionReveal className="text">a</BoxMotionReveal>
+      </HStack>
+      <Stack spacing="8" w="80%">
+        <Flex bg="gray.200" h="40rem" />
+        <Flex bg="gray.200" h="40rem" />
+
+        <Flex bg="gray.200" h="40rem" />
+      </Stack>
+      <Stack spacing="8" w="80%">
+        <Flex bg="gray.200" h="40rem" />
+        <Flex bg="gray.200" h="40rem" />
+
+        <Flex bg="gray.200" h="40rem" />
+      </Stack>
     </Flex>
   );
 };
